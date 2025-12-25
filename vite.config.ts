a0f06ea6d992f92503e587080938fa4632966c78
coizzz <jhesrytulud@gmail.com>
2025-12-25 05:21:00 +0000

    Fix GitHub Pages Build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/peabao-delight-website/",
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
   },
-}));
+  plugins: [react()],
+});
